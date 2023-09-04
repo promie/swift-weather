dccc2daf272ab4dbcb8f6ac8eb2d9e508e9fe6af
Promie Yutasane <promie.yutasane@gmail.com>
2023-09-04 21:21:09 +1000

    retrieving coordinates from getting location

--- SwiftWeather/Views/ContentView.swift
+++ SwiftWeather/Views/ContentView.swift
@@ -13,8 +13,16 @@ struct ContentView: View {
     
     var body: some View {
         VStack {
-            WelcomeView()
-                .environmentObject(locationManager)
+            
+            if let location = locationManager.location {
+                Text("Your coordinates are: \(location.longitude), \(location.latitude)")
+            }else {
+                if locationManager.isLoading {
+                    LoadingView()
+                }else {
+                    WelcomeView()
+                        .environmentObject(locationManager)                }
+            }
         }
         .background(Color(hue: 0.656, saturation: 0.787, brightness: 0.354))
         .preferredColorScheme(.dark)
---
+++ SwiftWeather/Views/LoadingView.swift
@@ -0,0 +1,22 @@
+//
+//  LoadingView.swift
+//  SwiftWeather
+//
+//  Created by Promie Yutasane on 4/9/2023.
+//
+
+import SwiftUI
+
+struct LoadingView: View {
+    var body: some View {
+        ProgressView()
+            .progressViewStyle(CircularProgressViewStyle(tint: .white))
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+    }
+}
+
+struct LoadingView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoadingView()
+    }
+}
