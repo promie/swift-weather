6f72ddf0edb0750b9d97b1c523210cd6d9c4cc4d
Promie Yutasane <promie.yutasane@gmail.com>
2023-09-04 20:25:46 +1000

    added project scaffold

---
+++ SwIft Weather/SwIft_WeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwIft_WeatherApp.swift
+//  SwIft Weather
+//
+//  Created by Promie Yutasane on 4/9/2023.
+//
+
+import SwiftUI
+
+@main
+struct SwIft_WeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SwIft WeatherUITests/SwIft_WeatherUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  SwIft_WeatherUITestsLaunchTests.swift
+//  SwIft WeatherUITests
+//
+//  Created by Promie Yutasane on 4/9/2023.
+//
+
+import XCTest
+
+final class SwIft_WeatherUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
