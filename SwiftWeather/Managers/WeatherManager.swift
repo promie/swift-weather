8789aa8cdf0d6d6385dec7485b5f53309a75977e
Promie Yutasane <promie.yutasane@gmail.com>
2023-09-06 05:30:44 +1000

    configured the xcconfig to store API key

--- SwiftWeather/Managers/WeatherManager.swift
+++ SwiftWeather/Managers/WeatherManager.swift
@@ -9,7 +9,12 @@ import CoreLocation
 
 class WeatherManager {
     func getCurrentWeather(latitude: CLLocationDegrees, longitude: CLLocation) async throws {
-        guard let url = URL(string: "https://api.openweathermap.org/data/3.0/weather?lat=\(latitude)&lon=\(longitude)&appid={API key}") else { fatalError("Missing URL")}
+        
+        guard let infoDictionary: [String: Any] = Bundle.main.infoDictionary else { return }
+        let apiKey: String = infoDictionary["OpenWeatherAPIKey"] as? String ?? ""
+                
+    
+        guard let url = URL(string: "https://api.openweathermap.org/data/3.0/weather?lat=\(latitude)&lon=\(longitude)&appid=\(apiKey)") else { fatalError("Missing URL")}
         
     }
 }
