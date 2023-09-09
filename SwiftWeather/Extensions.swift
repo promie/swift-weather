33b3e5011b0191bcfa54b22d7b0aca3692da1738
Promie Yutasane <promie.yutasane@gmail.com>
2023-09-09 10:48:42 +1000

    added the images and added the extension to convert weather images in

---
+++ SwiftWeather/Extensions.swift
@@ -0,0 +1,14 @@
+//
+//  Extensions.swift
+//  SwiftWeather
+//
+//  Created by Promie Yutasane on 9/9/2023.
+//
+
+import Foundation
+
+extension Double {
+    func roundDouble() -> String {
+        return String(format: "%.0f", self)
+    }
+}
